1b8b80b0309434cd72a480f6823fe3b9f68e95c6
danrtsey.wy <danrtsey.wy@alibaba-inc.com>
2018-12-10 19:35:51 +0800

    BLINK-18123473 : Support saving gc log to file
    Summary: Support saving gc log to file
    
    Reviewers: 瓦力
    
    Reviewed By: 瓦力
    
    Subscribers: P710480
    
    Differential Revision: https://aone.alibaba-inc.com/code/D795374

--- flink-dist/src/main/flink-bin/bin/flink-daemon.sh
+++ flink-dist/src/main/flink-bin/bin/flink-daemon.sh
@@ -90,8 +90,12 @@ FLINK_LOG_PREFIX="${FLINK_LOG_DIR}/flink-${FLINK_IDENT_STRING}-${DAEMON}-${id}-$
 log="${FLINK_LOG_PREFIX}.log"
 out="${FLINK_LOG_PREFIX}.out"
 code="${FLINK_LOG_PREFIX}.code"
+gclog="${FLINK_LOG_PREFIX}-gc.log"
 
-log_setting=("-Dlog.file=${log}" "-Dcode.file=${code}" "-Dlog4j.configuration=file:${FLINK_CONF_DIR}/log4j.properties" "-Dlogback.configurationFile=file:${FLINK_CONF_DIR}/logback.xml")
+log_setting=("-Dlog.file=${log}" "-Dcode.file=${code}" \
+"-Dlog4j.configuration=file:${FLINK_CONF_DIR}/log4j.properties" \
+"-Dlogback.configurationFile=file:${FLINK_CONF_DIR}/logback.xml" \
+"-Xloggc:${gclog}")
 
 JAVA_VERSION=$(${JAVA_RUN} -version 2>&1 | sed 's/.*version "\(.*\)\.\(.*\)\..*"/\1\2/; 1q')
 
