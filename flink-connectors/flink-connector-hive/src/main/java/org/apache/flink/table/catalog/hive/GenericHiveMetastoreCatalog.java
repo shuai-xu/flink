bff65192cc28e39c06488d63230bc0a476cdfde6
bowen.li <bowen.li@alibaba-inc.com>
2019-01-24 21:35:25 -0800

    [BLINK-18826828] add an in-memory catalog to GenericHiveMetastoreCatalog to support Flink temp tables
    Summary:
    there are already unit tests in GenericHiveMetastoreCatalogTest that can ensure behaviors haven't changed after the PR.
    
    will add more comprehensive tests in a separate PR.
    
    Reviewers: 乔燃, xuefu.z
    
    Reviewed By: xuefu.z
    
    Subscribers: P710480
    
    Differential Revision: https://aone.alibaba-inc.com/code/D831428

--- flink-connectors/flink-connector-hive/src/main/java/org/apache/flink/table/catalog/hive/GenericHiveMetastoreCatalog.java
+++ flink-connectors/flink-connector-hive/src/main/java/org/apache/flink/table/catalog/hive/GenericHiveMetastoreCatalog.java
@@ -18,6 +18,7 @@
 
 package org.apache.flink.table.catalog.hive;
 
+import org.apache.flink.table.api.DatabaseAlreadyExistException;
 import org.apache.flink.table.api.DatabaseNotExistException;
 import org.apache.flink.table.api.TableAlreadyExistException;
 import org.apache.flink.table.api.TableNotExistException;
@@ -28,6 +29,8 @@ import org.apache.flink.table.catalog.CatalogDatabase;
 import org.apache.flink.table.catalog.CatalogPartition;
 import org.apache.flink.table.catalog.CatalogTable;
 import org.apache.flink.table.catalog.CatalogView;
+import org.apache.flink.table.catalog.FlinkInMemoryCatalog;
+import org.apache.flink.table.catalog.FlinkTempTable;
 import org.apache.flink.table.catalog.ObjectPath;
 import org.apache.flink.table.plan.stats.TableStats;
 
@@ -38,6 +41,7 @@ import org.apache.thrift.TException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -47,13 +51,22 @@ import java.util.List;
 public class GenericHiveMetastoreCatalog extends HiveCatalogBase {
 	private static final Logger LOG = LoggerFactory.getLogger(GenericHiveMetastoreCatalog.class);
 
+	// This only serves to fill the in-memory catalog with a name, and should not be used anywhere else
+	private static final String BUILTIN_CATALOG_NAME = "generic-hive-metastore-builtin";
+
+	private final FlinkInMemoryCatalog inMemoryCatalog;
+
 	public GenericHiveMetastoreCatalog(String catalogName, String hiveMetastoreURI) {
 		super(catalogName, hiveMetastoreURI);
+
+		inMemoryCatalog = new FlinkInMemoryCatalog(BUILTIN_CATALOG_NAME);
 		LOG.info("Created GenericHiveMetastoreCatalog '{}'", catalogName);
 	}
 
 	public GenericHiveMetastoreCatalog(String catalogName, HiveConf hiveConf) {
 		super(catalogName, hiveConf);
+
+		inMemoryCatalog = new FlinkInMemoryCatalog(BUILTIN_CATALOG_NAME);
 		LOG.info("Created GenericHiveMetastoreCatalog '{}'", catalogName);
 	}
 
@@ -61,44 +74,99 @@ public class GenericHiveMetastoreCatalog extends HiveCatalogBase {
 
 	@Override
 	public CatalogTable getTable(ObjectPath path) throws TableNotExistException {
-		Table hiveTable = getHiveTable(path);
+		if (inMemoryCatalog.tableExists(path)) {
+			return inMemoryCatalog.getTable(path);
+		} else {
+			Table hiveTable = getHiveTable(path);
 
-		return GenericHiveMetastoreCatalogUtil.createCatalogTable(hiveTable);
+			return GenericHiveMetastoreCatalogUtil.createCatalogTable(hiveTable);
+		}
+	}
+
+	@Override
+	public void dropTable(ObjectPath path, boolean ignoreIfNotExists) throws TableNotExistException {
+		if (inMemoryCatalog.tableExists(path)) {
+			inMemoryCatalog.dropTable(path, ignoreIfNotExists);
+		} else {
+			super.dropTable(path, ignoreIfNotExists);
+		}
+	}
+
+	@Override
+	public List<ObjectPath> listTables(String dbName) throws DatabaseNotExistException {
+		List<ObjectPath> result = new ArrayList<>();
+		result.addAll(inMemoryCatalog.listTables(dbName));
+		result.addAll(super.listTables(dbName));
+
+		return result;
+	}
+
+	@Override
+	public List<ObjectPath> listAllTables() {
+		List<ObjectPath> result = new ArrayList<>();
+		result.addAll(inMemoryCatalog.listAllTables());
+		result.addAll(super.listAllTables());
+
+		return result;
+	}
+
+	@Override
+	public boolean tableExists(ObjectPath path) {
+		return inMemoryCatalog.tableExists(path) || super.tableExists(path);
 	}
 
 	// ------ tables ------
 
 	@Override
 	public void createTable(ObjectPath path, CatalogTable table, boolean ignoreIfExists) throws TableAlreadyExistException, DatabaseNotExistException {
-		try {
-			if (tableExists(path)) {
-				if (!ignoreIfExists) {
-					throw new TableAlreadyExistException(catalogName, path.getFullName());
-				}
-			} else {
-				// Testing shows that createHiveTable() API in Hive 2.3.4 doesn't throw UnknownDBException as it claims
-				// Thus we have to manually check if the db exists or not
-				if (!dbExists(path.getDbName())) {
-					throw new DatabaseNotExistException(catalogName, path.getDbName());
-				}
-
-				client.createTable(GenericHiveMetastoreCatalogUtil.createHiveTable(path, table));
+		// Check no table exists with such name in either in-memory or hive
+		if (tableExists(path)) {
+			if (!ignoreIfExists) {
+				throw new TableAlreadyExistException(catalogName, path.getFullName());
 			}
-		} catch (TException e) {
-			throw new FlinkHiveException(
-				String.format("Failed to create table %s", path.getFullName()), e);
+		} else {
+			if (table instanceof FlinkTempTable) {
+				inMemoryCatalog.createTable(path, table, ignoreIfExists);
+			} else {
+				try {
+					// Testing shows that createHiveTable() API in Hive 2.3.4 doesn't throw UnknownDBException as it claims
+					// Thus we have to manually check if the db exists or not
+					if (!dbExists(path.getDbName())) {
+						throw new DatabaseNotExistException(catalogName, path.getDbName());
+					}
+					client.createTable(GenericHiveMetastoreCatalogUtil.createHiveTable(path, table));
+				} catch (TException e) {
+					throw new FlinkHiveException(String.format("Failed to create table %s", path.getFullName()), e);
+				}
+			}
 		}
 	}
 
 	@Override
 	public void alterTable(ObjectPath path, CatalogTable newTable, boolean ignoreIfNotExists) throws TableNotExistException {
-		// alter_table() requires the table to have a valid location, which it doesn't in this case
-		// Thus we have to translate alterTable() into (dropTable() + createTable())
-		if (tableExists(path)) {
-			dropTable(path, false);
-			createTable(path, newTable, false);
-		} else if (!ignoreIfNotExists) {
-			throw new TableNotExistException(catalogName, path.getFullName());
+		if (!tableExists(path)) {
+			if (!ignoreIfNotExists) {
+				throw new TableNotExistException(catalogName, path.getFullName());
+			}
+		} else {
+			if (inMemoryCatalog.tableExists(path)) {
+				if (newTable instanceof FlinkTempTable) {
+					inMemoryCatalog.alterTable(path, newTable, ignoreIfNotExists);
+				} else {
+					throw new IllegalArgumentException(
+						String.format("Table %s is a FlinkTempTable, the new table is a CatalogTable.", path.getFullName()));
+				}
+			} else {
+				if (!(newTable instanceof FlinkTempTable)) {
+					// IMetastoreClient.alter_table() requires the table to have a valid location, which it doesn't in this case
+					// Thus we have to translate alterTable() into (dropTable() + createTable())
+					dropTable(path, false);
+					createTable(path, newTable, false);
+				} else {
+					throw new IllegalArgumentException(
+						String.format("Table %s is a CatalogTable, the new table is a FlinkTempTable.", path.getFullName()));
+				}
+			}
 		}
 	}
 
@@ -117,6 +185,31 @@ public class GenericHiveMetastoreCatalog extends HiveCatalogBase {
 	@Override
 	public void alterView(ObjectPath viewPath, CatalogView newView, boolean ignoreIfNotExists) {
 		throw new UnsupportedOperationException();
+	}
+
+	// ------ databases ------
+
+	@Override
+	public void createDatabase(String dbName, CatalogDatabase db, boolean ignoreIfExists) throws DatabaseAlreadyExistException {
+		inMemoryCatalog.createDatabase(dbName, db, ignoreIfExists);
+		super.createDatabase(dbName, db, ignoreIfExists);
+	}
+
+	@Override
+	public void alterDatabase(String dbName, CatalogDatabase newDatabase, boolean ignoreIfNotExists) throws DatabaseNotExistException {
+		inMemoryCatalog.alterDatabase(dbName, newDatabase, ignoreIfNotExists);
+		super.alterDatabase(dbName, newDatabase, ignoreIfNotExists);
+	}
+
+	@Override
+	public void renameDatabase(String dbName, String newDbName, boolean ignoreIfNotExists) throws DatabaseNotExistException {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void dropDatabase(String dbName, boolean ignoreIfNotExists) throws DatabaseNotExistException {
+		inMemoryCatalog.dropDatabase(dbName, ignoreIfNotExists);
+		super.dropDatabase(dbName, ignoreIfNotExists);
 	}
 
 	// ------ table and column stats ------
